3a4df33ec1acd5aca7482a69547af72047f5ad11
Shraddha <shraddhasahu712@gmail.com>
2026-08-05 02:21:25 +0530

    Added History saver festure

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from utils import get_username
+from utils import get_username, save_history
 from github_api import fetch_github_profile
 from display import display_profile
 def main():
     username = get_username()
     response = fetch_github_profile(username)
     if response is not None:
+        save_history(username,"Success")
         display_profile(response)
+    else:
+        save_history(username,"Failed")
     
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
+from datetime import datetime
 def get_username():
     """Take GitHub username from the user."""
     return input("Enter GitHub username: ")
+
+def save_history(username, status):
+    """Save the searched username to history.txt with a timestamp."""
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    
+    with open("history.txt", "a") as file:
+        file.write(f"[{timestamp}] {status} | {username}\n")
+        
+        
+      
+        
